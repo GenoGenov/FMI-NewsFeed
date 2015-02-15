27530a0209263d1a66803e3f19f093a489635200
Geno Genov <dirtydances@gmail.com>
2015-02-15 15:16:59 +0200

    methods + messages exclude muted

--- NewsFeed.Services/server/config/express.js
+++ NewsFeed.Services/server/config/express.js
@@ -11,6 +11,7 @@ module.exports = function (app, config) {
     app.use(function (req, res, next) {
         res.header("Access-Control-Allow-Origin", req.headers.origin);
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS, DELETE");
         res.header("Access-Control-Allow-Credentials", "true");
         next();
     });
