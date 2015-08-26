390bf0da158ec585aa6b574aa442e0a4cd72bbf7
lovechguy <lovechguy@gmail.com>
2015-08-26 22:11:51 +0300

    Refactor to use angularjs

---
+++ Newsfeed.Client/Newsfeed.Client/js/app.js
@@ -0,0 +1,23 @@
+﻿'use strict';
+
+var app = angular.module('newsfeed', ['ngRoute', 'ngResource'])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/login', {
+                templateUrl: 'views/login.html',
+                controller: 'LoginController'
+            })
+            .when('/newsfeed', {
+                templateUrl: 'views/newsfeed.html',
+                controller: 'NewsfeedController'
+            })
+            //.when('/add-manufacturer', {
+            //    templateUrl: 'views/partials/add-manufacturer.html',
+            //    controller: 'AddManufacturerController'
+            //})
+            //.when('/add-car', {
+            //    templateUrl: 'views/partials/add-car.html',
+            //    controller: 'AddCarController'
+            //})
+            .otherwise({ redirectTo: '/' });
+    }])
---
+++ Newsfeed.Client/Newsfeed.Client/js/controllers/NewsfeedController.js
@@ -0,0 +1,57 @@
+﻿'use strict';
+
+app.controller('NewsfeedController', function NewsfeedController($scope, $http, $location) {
+
+    var userData = JSON.parse(localStorage.getItem('userData'));
+
+    $scope.getAllMessages = function () {
+        $http.get('http://localhost:3030/api/messages', {withCredentials: true})
+            .then(function (data) {
+                console.log(data);
+            }, function () {
+
+            })
+    }
+
+    $scope.postMessage = function(){
+        console.log({ content: $scope.userMessage });
+        $http.post('http://localhost:3030/api/messages/create', { content: $scope.userMessage })
+            .then(function (res) {
+                console.log(res);
+            }, function () {
+            })
+    }
+});
+
+
+//var messageData = {
+//    //author: currentUserName,
+//    content: messageText
+//};
+//console.log(messageData);
+//$.ajax({
+//    type: "POST",
+//    url: 'http://localhost:3030/api/messages/create',
+//    data: JSON.stringify(messageData),
+//    success: getMessages,
+//    error: function () { alert('Error sending message') },
+//    contentType: 'application/json',
+//    dataType: 'json',
+//    xhrFields: {
+//        withCredentials: true
+//    },
+//});
+
+
+//function getMessages() {
+//    $.ajax({
+//        type: "GET",
+//        url: "http://localhost:3030/api/messages",
+//        success: function (data) { updateMessages(data) },//updateMessages(data),
+//        contentType: 'application/json',
+//        dataType: 'json',
+//        xhrFields: {
+//            withCredentials: true
+//        }
+//    });
+//}
