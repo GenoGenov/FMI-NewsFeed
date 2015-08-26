f16214d5448dc83d252397cb599222aa73be0b23
lovechguy <lovechguy@gmail.com>
2015-08-27 00:42:08 +0300

    users rating

--- Newsfeed.Client/Newsfeed.Client/js/controllers/UsersRatingController.js
+++ Newsfeed.Client/Newsfeed.Client/js/controllers/UsersRatingController.js
@@ -1,7 +1,16 @@
 ﻿'use strict';
 
 app.controller('UsersRatingController', function UsersRatingController($scope, $http, $location) {
+    $scope.allUsers = [];
 
-    
+    $scope.getAllUsers = function () {
+        $http.get('http://localhost:3030/api/users')
+            .then(function (res) {
+                console.log(res);
+                $scope.allUsers = res.data;
+            }, function () {
+
+            })
+    }    
 });
 
